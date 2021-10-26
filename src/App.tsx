197a96f6dcc7be4be127d4b363d5061c2139429b
dbwcooper <dbwencookie@gmail.com>
2021-10-26 21:20:14 +0800

    feat: add sandpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import TailwindButton from './TailwindButton';
 
+import CustomSandpack from './sandpack';
+
 export default function App() {
   return (
     <div className='App'>
-      <TailwindButton />
+      <CustomSandpack>
+        <div className='App'>
+          <button className='px-8 p-2 rounded-lg cursor-pointer text-white bg-purple-400 border border-white hover:bg-primary-v1 hover:text-white transition-colors duration-200 '>
+            Custom Button
+          </button>
+        </div>
+      </CustomSandpack>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import './styles/algolia.css';
+import './styles/index.css';
+import './styles/sandpack.css';
+import '@codesandbox/sandpack-react/dist/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
